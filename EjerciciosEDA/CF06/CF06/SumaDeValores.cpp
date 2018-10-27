246717984bb23e79c4ed8a55bdce06958d83e291
Rober <omegaskyres9@gmail.com>
2018-10-27 13:51:10 +0200

    Commit

--- EjerciciosEDA/CF06/CF06/SumaDeValores.cpp
+++ EjerciciosEDA/CF06/CF06/SumaDeValores.cpp
@@ -9,28 +9,31 @@ using namespace std;
 
 
 void resuelveCaso() {
-	int numeros, maximo = 0, posMax = 0, suma = 0;
+	int numeros, maximo = 0, posMax = 0, suma = 0, i = 0;
 	cin >> numeros;
 	vector<int> datos(numeros);
 	for (int i = 0; i < numeros; i++) {
 		cin >> datos[i];
 	}
-	maximo = datos[0];
 	if (datos.empty()) {
 		suma = 0;
 	}
 	else {
-		for (int i = 0; i < numeros - 1; i++) {
-			if (datos[i] < datos[i + 1]) {
-				maximo = datos[i + 1];
-				posMax = i;
+		maximo = datos[0];
+		for (int i = 1; i < numeros; i++) { //Calcula el Maximo y empiezas a mirar desde el 1 porque el primer elemento siempre va a ser el maximo
+			if (maximo < datos[i]) {	
+				maximo = datos[i];
 			}
 		}
-		if (datos[posMax] != 0) {
-			for (int i = 0; i < numeros; i++) {
-				if (datos[i] != maximo) suma += datos[i];
+		if (!datos.empty() && maximo != 0){
+			while (i < datos.size()) {
+				if (datos[i] != maximo) {
+					suma += datos[i];
+				}
+				i++;
 			}
 		}
+
 		
 			
 	}
