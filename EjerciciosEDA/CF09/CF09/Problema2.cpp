5f937d193564ef1496791b81c61084baa3d2443c
Rober <omegaskyres9@gmail.com>
2018-10-29 23:27:31 +0100

    Commit

--- EjerciciosEDA/CF09/CF09/Problema2.cpp
+++ EjerciciosEDA/CF09/CF09/Problema2.cpp
@@ -1,18 +1,43 @@
-//NOMBRE y APELLIDOS
+//Roberto Torres Prensa	
 
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
-
+void resuelve(vector<int> &datos) {
+	int contador = 0;
+	for (int i = 0; i < datos.size(); i++) {
+		if (datos[i] % 2 == 0) {
+			datos[contador] = datos[i];
+			contador++;
+		}
+		else {
+			datos[i] = 0;
+		}
+	}
+	datos.resize(contador);
+	for (int i = 0; i < datos.size(); i++) { // o si no cambiar datos.size por contador
+		if (datos[i] != 0) {
+			cout << datos[i] << " ";
+		}
+		else cout << " ";
+	}
+	cout << endl;
+}
 
 void resuelveCaso() {
-	//resuelve aqui tu caso
-	   //Lee los datos
-	   //Calcula el resultado
-	   //Escribe el resultado
+	int nElementos;
+	cin >> nElementos;
+	vector<int> datos(nElementos);
+	for (int i = 0; i < nElementos; i++) {
+		cin >> datos[i];
+	}
+	resuelve(datos);
 }
+
+
 
 int main() {
 	// Para la entrada por fichero.
