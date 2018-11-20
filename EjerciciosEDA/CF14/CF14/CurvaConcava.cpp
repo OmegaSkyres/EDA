0fb8cbf9da65e656e7e7494009c5821bfa8b8fee
Rober <omegaskyres9@gmail.com>
2018-11-21 00:55:06 +0100

    Commit

--- EjerciciosEDA/CF14/CF14/CurvaConcava.cpp
+++ EjerciciosEDA/CF14/CF14/CurvaConcava.cpp
@@ -3,16 +3,44 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 
 
 void resuelveCaso() {
+	vector<int> vector;
+	int nElementos;
+	cin >> nElementos;
+	for (int i = 0; i < nElementos; i++) {
+		cin >> vector[i];
+	}
+}
+
+	int buscaBin(vector<int> vector, int a, int b) {
+		// Pre : v esta ordenado entre 0 . . longitud (v)1
+		// ( 0 <= a <= l o n g i t u d ( v ) ) && ( -1 <= b <= longitud(v) - 1) && (a <= b + 1)
+			// todos l o s elementos a la izquie rda de ’a ’ son <= x
+			// todos l o s elementos a la derecha de ’b ’ son > x
+			int p, m;
+		if (a == b + 1)
+			p = a - 1;
+		else { // a <= b
+			m = (a + b) / 2;
+			if (vector[m] <= x)
+				p = buscaBin(vector, x, m + 1, b);
+	
+			else
+				p = buscaBin(vector, x, a, m - 1);
+		}
+		return p;
+		// Post : devuelve el mayor i (0 <= i <= longitud(v) - 1) que cumple v[i] <= x
+		// s i x es menor que todos l o s elementos de v , devuelve 1
+	}
 	//resuelve aqui tu caso
 	   //Lee los datos
 	   //Calcula el resultado
-	   //Escribe el resultado
-}
+	   //Escribe el resultado}
 
 int main() {
 	// Para la entrada por fichero.
