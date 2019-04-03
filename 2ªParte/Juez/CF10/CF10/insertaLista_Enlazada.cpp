983de0c2c6bda3a8e9db79c84c81ed7cef671aca
Rober <omegaskyres9@gmail.com>
2019-04-03 11:23:53 +0200

    Commit

---
+++ 2ªParte/Juez/CF10/CF10/insertaLista_Enlazada.cpp
@@ -0,0 +1,74 @@
+﻿//Roberto Torres Prensa
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+#include "queue_eda.h"
+
+template <class T>
+class Lista : public queue<T> {
+	using Nodo = typename queue<T>::Nodo;  // para poder usar Nodo aqu�
+public:
+	void print(std::ostream & o = std::cout) const {
+		if (!this->empty()) {// la lista no est� vac�a
+			Nodo * nodo = this->prim;
+			for (int i = 0; i < this->nelems; i++) {
+				cout << nodo->elem << " ";
+				nodo = nodo->sig;
+			}
+		}
+	}
+
+	// Duplicar los nodos de una lista
+	void insertaEnListaEnlazada(Lista<int> lista2) {
+		
+
+		// COMPLETAR
+
+
+	}
+};
+
+template <class T>
+inline std::ostream& operator<<(std::ostream & o, Lista<T> const& a) {
+	a.print(o);
+	return o;
+}
+
+bool resuelveCaso() {
+	int n, elementosInsertar, posicion;
+	std::cin >> n;
+	if (!cin) return false;
+	Lista<int> lista;
+	Lista<int> elementos;
+	// creamos la lista
+	for(int i = 0; i < n; i++) {
+		lista.push(i);
+	}
+	cin >> elementosInsertar >> posicion;
+	for (int i = 0; i < elementosInsertar; i++) {
+		elementos.push(i);
+	}
+	// insertamos en la lista
+	lista.insertaEnListaEnlazada(lista);
+	// y la mostramos
+	std::cout << lista << '\n';
+	return true;
+}
+
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+	return 0;
+}
---
+++ 2ªParte/Juez/CF19/CF19/nodos.cpp
@@ -0,0 +1,96 @@
+// Altura de un árbol
+
+// Construye un árbol binario a partir de la entrada y después
+// calcula de forma recursiva su altura
+// O(N)
+
+// versión con extensión de la clase bintree del TAD dado en clase (Alberto Verdejo)
+
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+
+#include "bintree_eda.h"
+using namespace std;
+
+/*
+template <class T>
+class bintree_ext : public bintree<T> {
+	using Link = typename bintree<T>::Link;
+	public:
+	bintree_ext() : bintree<T>() {}
+	bintree_ext(bintree_ext<T> const& l, T const& e, bintree_ext<T> const& r) : bintree<T>(l, e, r) {}
+	int altura() {
+		return altura(this->raiz);
+	}
+
+	private:
+	int altura(Link r) {
+		if (r == nullptr) {
+			return 0;
+		}
+		else {
+			return 1 + std::max(altura(r->left), altura(r->right));
+		}
+	}
+};	
+
+
+// lee un árbol binario de la entrada estándar
+// Transparencia 10 del tema "Tipos de datos arborescentes" 
+template <typename T>
+bintree_ext<T> leerArbolext(T vacio) {
+	T raiz;
+	std::cin >> raiz;
+	if (raiz == vacio) { // es un árbol vacío
+		return {};
+	}
+	else { // leer recursivamente los hijos
+		auto iz = leerArbolext(vacio);
+		auto dr = leerArbolext(vacio);
+		return { iz, raiz, dr };
+	}
+}
+*/
+
+int calculaAltura(bintree<char> arbol) {
+	if (arbol.empty()) {
+		return 0;
+	}
+	else {
+		return 1 + std::max(calculaAltura(arbol.left()), calculaAltura(arbol.right()));
+	}
+}
+
+void resuelveCaso() {
+	int nElementos;
+	bintree<char> arbol;
+	arbol = leerArbol('.');
+	cout << calculaAltura(arbol) << endl;
+}
+
+
+
+	int main() {
+		// Para la entrada por fichero.
+#ifndef DOMJUDGE
+		std::ifstream in("casos.txt");
+		auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+
+		unsigned int numCasos;
+		std::cin >> numCasos;
+		// Resolvemos
+		for (int i = 0; i < numCasos; ++i) {
+			resuelveCaso();
+		}
+
+
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+		std::cin.rdbuf(cinbuf);
+		system("PAUSE");
+#endif
+
+		return 0;
+	}
