c5be5305ffa5c8e29ef05fc7e46994ffbf8b3bb9
Rober <omegaskyres9@gmail.com>
2019-05-07 16:56:13 +0200

    CommitR

--- 2ªParte/Juez/CF29/CF29/Source.cpp
+++ 2ªParte/Juez/CF29/CF29/Source.cpp
@@ -12,7 +12,7 @@ void resuelveCaso() {
 	unordered_map<int, int> emisiones;
 	int numCapitulos, capitulo, repeticiones = 0, contador = 0, longMax = 0;
 	cin >> numCapitulos;
-	for (int i = 0; i < numCapitulos; i++) {
+	for (int i = 0; i <= numCapitulos; i++) {
 		cin >> capitulo;
 		if (emisiones.count(capitulo) > 0 && repeticiones <= emisiones[capitulo]) { // Miramos si existe alguna emision de ese episodio y si el numero de repeticiones de la emision es menor o igual al numero de emisiones de ese episodio.
 			contador = 0;
